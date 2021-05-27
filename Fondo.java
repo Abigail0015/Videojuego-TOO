d8c0957392b82e07642d92e92b09da3f37df5b42
CristoSparda <cristo.n6-@hotmail.com>
2021-05-26 23:36:30 -0500

    Test comit

---
+++ Fondo.java
@@ -0,0 +1,8 @@
+import greenfoot.*;
+public class Fondo extends Actor
+{
+    public void act() 
+    {
+        setImage("images/first.png");
+    }    
+}
---
+++ Level1.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  
+
+public class Level1 extends World
+{
+    public Level1()
+    {    
+        super(600, 400, 1); 
+        this.addObject(new Fondo(),2,2);
+    }
+    
+}
